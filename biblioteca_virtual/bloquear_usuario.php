578d455ad05018e40f2c6788eff4c2f3f86eb75b
diegopracticasfuen-maker <diegopracticasfuen@sanjuanbosco.net>
2026-05-19 18:49:53 +0200

    Delete biblioteca_virtual directory

--- biblioteca_virtual/bloquear_usuario.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'db.php';
-$id = $_GET['id'];
-$conexion->query("UPDATE usuarios SET estado = 'bloqueado' WHERE id = $id");
-header("Location: admin_usuarios.php");
-?>
--- biblioteca_virtual/conexion/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conexion = new mysqli("localhost", "root", "", "biblioteca_virtual");
-
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-}
-?>
